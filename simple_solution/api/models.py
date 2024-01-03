5fa318020ddeb68948bb87e95f780c36f7b5accf
Алан Джелиев <djalan84@mail.ru>
2024-01-03 19:51:22 +0300

    Refactor: Order model modified. OrderItem model added

--- simple_solution/api/models.py
+++ simple_solution/api/models.py
@@ -14,11 +14,34 @@ class Item(models.Model):
 
 
 class Order(models.Model):
-    items = models.ManyToManyField('Item')
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    address = models.CharField(max_length=250)
+    postal_code = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
 
-    def calculate_total_cost(self):
-        total_cost = 0
-        for item in self.items.all():
-            total_cost += item.price
-        return total_cost
+    class Meta:
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return f'Order {self.id} | {self.created_at.strftime("%d.%m.%Y %H:%M")}'
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, related_name='order_items', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f'{self.id}'
+
+    def get_cost(self):
+        return self.price * self.quantity
